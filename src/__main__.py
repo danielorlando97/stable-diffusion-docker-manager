971722cf90fca3208eab78ded1ea5d9db21eccae
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-03-13 11:49:31 -0400

    first try

---
+++ src/__main__.py
@@ -0,0 +1,17 @@
+# make sure you're logged in with `huggingface-cli login`
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5")
+pipe = pipe.to("mps")
+
+# Recommended if your computer has < 64 GB of RAM
+pipe.enable_attention_slicing()
+
+prompt = "a photo of an astronaut riding a horse on mars"
+
+# First-time "warmup" pass (see explanation above)
+_ = pipe(prompt, num_inference_steps=1)
+
+# Results match those from the CPU device after the warmup pass.
+image = pipe(prompt).images[0]
---
+++ train.sh
@@ -0,0 +1,17 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export DATA_DIR="./image-dataset"
+export DISABLE_TELEMETRY=YES
+
+accelerate launch train.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$DATA_DIR \
+  --learnable_property="object" \
+  --placeholder_token="<target>" --initializer_token="toy" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --max_train_steps=3000 \
+  --learning_rate=5.0e-04 --scale_lr \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --output_dir="textual_inversion_target"
